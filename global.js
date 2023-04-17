da2bfa065682f2dcad1c1553703de68f6e039074
josek <kinyuajoe4@gmail.com>
2023-04-17 22:14:12 +0300

    first commit

---
+++ global.js
@@ -0,0 +1,11 @@
+// setTimeout(() => {
+//     console.log('in the set time out'); 
+// }, 3000);
+
+// const rep = setInterval(()=>{
+//     console.log('AFTER 3 SEC');   
+// },1000);
+// console.log(__dirname);
+// console.log(__filename);
+const inkk = require('./test');
+console.log(inkk);
---
+++ test.js
@@ -0,0 +1,7 @@
+const people =['JOS','ken','henry'];
+const ages= ['12','23','14']
+
+console.log(people);
+module.exports= {
+    people,ages
+};
