779614d06aa2508a77cca0d90ac0af252afc3868
josek <kinyuajoe4@gmail.com>
2023-04-21 22:40:09 +0300

    final change

--- app.js
+++ app.js
@@ -7,26 +7,33 @@ app.set('view engine','ejs');
 //listen for requests
 app.listen(3000);
 app.get('/',(req,res)=>{
+    const blogs=[
+     { title:'JOSEE' , snippet:'asdfgh'},
+     { title:'joe' , snippet:'asdfgh'},
+     { title:'kinyua' , snippet:'asdfgh'}
+    ];
     //res.send('<p>hello john</p>')
     //res.sendFile('./docs/maji.html',{root:__dirname});  
-    res.render('maji');
+    res.render('maji',{title:'Home',blogs});
 });
 
 app.get('/about',(req,res)=>{
     //res.send('<p>hello john about page</p>')
-    res.render('work'); 
+    res.render('work',{title:'About'}); 
+});
+
+app.get('/create/blog',(req,res)=>{
+    //res.send('<p>hello john about page</p>')
+    res.render('blog',{title:'Create Blog'}); 
 });
 //redirects
 app.get('/about-us',(req,res)=>{
     res.redirect('/about');
 });
 
+
 //404 error
 app.use((req,res)=>{
-    res.status(400).res.render('oops');
+    res.status(400).res.render('oops',{title:'404'});
 });
 
-app.get('/blog/create',(req,res)=>{
-    //res.send('<p>hello john about page</p>')
-    res.render('blog'); 
-});
