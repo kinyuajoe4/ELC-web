35c133493a10c7e18a7d5fb97c1dc1c93698481a
josek <kinyuajoe4@gmail.com>
2023-05-07 23:44:48 +0300

    ln

--- test.js
+++ test.js
@@ -1,8 +1,24 @@
-const people =['JOS','ken','henry'];
-const ages= ['12','23','14']
+console.log('hello there');
+const jsonfile= require ('jsonfile');
+const moment= require ('moment');
+const simpleGit= require ('simple-git');
+const makeCommit =(x,y)=>{
+    const FILE_PATH='./data.json';
+const DATE = moment().subtract(1,'d').add(1,'d').add(x,'w').add(y,'d').format();
+const data ={
+    date:DATE
+}
+jsonfile.writeFile(FILE_PATH,data,()=>{
+    simpleGit().add([FILE_PATH]).commit(DATE,{'--date':DATE}).push();
+    console.log('hello there');
+    simpleGit.push();
+});
 
-console.log(people);
-module.exports= {
-    people,ages
-};
+}
+makeCommit(1,6);
+
+
+
+
+
 
